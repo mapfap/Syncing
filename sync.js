f5d4c1861465ea85bddfc7e24204aa47c47458b0
mapfap <mapkabb@gmail.com>
2014-10-22 02:43:35 +0700

    Chat mode and Bootstraping

--- sync.js
+++ sync.js
@@ -6,32 +6,27 @@ exports.server = function() {
   var app = express();
   var server = require("http").createServer(app);
   var io = require("socket.io").listen(server);
-  // io.set("log level", 1);
+  io.set("log level", 1);
 
   app.use(express.bodyParser({uploadDir: __dirname + "/static/upload"}));
   app.post("/upload", function(req, res){
-    // console.log(req.body);
-    // console.log(req.files);
     var oldFilename = req.files.file.originalFilename;
     var newFilename = req.files.file.ws.path.match(/\/static\/upload\/(\S*)/)[1];
     var body = {old: oldFilename, new: "/upload/" + newFilename};
 
     res.json(body);
-    io.sockets.emit("sync", JSON.stringify(body));
+    io.sockets.emit("chat", JSON.stringify(body), "a");
   });
 
-  // var fileupload = require("fileupload").createFileUpload("/tmp").middleware
-
-  // app.post("/upload", fileupload, function(req, res) {
-     // console.log(req.body)
-  // })
-
   io.on("connection", function(client) {
-
     console.log(client.handshake.address.address + " connected");
 
     client.on("sync", function(sync) {
       io.sockets.emit("sync", sync);
+    });
+
+    client.on("chat", function(msg) {
+      io.sockets.emit("chat", msg);
     });
   });
   app.use(express.static(__dirname + "/static"));
